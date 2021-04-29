504de7217efee15718286df9a8c081185c49f800
Alexandre Masy <alexandre@infinity-mobile.io>
2021-04-29 04:25:01 +0200

    Tests: Currency

--- __tests__/currency.js
+++ __tests__/currency.js
@@ -5,16 +5,23 @@ beforeAll(() => {
   basil.use(install)
 })
 
-describe('currency', () => {
-  it('1 -> €1.00', () => {
-    expect(basil.i18n.currency(1)).toBe('€1.00');
-  });
-  
-  it('123.456 -> €123.46', () =>{
-    expect(basil.i18n.currency(123.456)).toBe('€123.46');
-  })
+let tests = [
+  { in: 1,                   out: '€1.00', options: {}},
+  { in: 123456789.123456789, out: '€123,456,789.12', options: {}},
+  { in: 123456789.123456789, out: '123 456 789,12 €', options: { locale: 'fr' }},
+  { in: 123456789.123456789, out: '€123.46M', options: { compact: true }},
+  { in: 123456789.123456789, out: '€123,456,789.123', options: { fraction: 3 }},
+  { in: 123456789.123456789, out: '€123456789.123', options: { fraction: 3, group: false }},
+]
 
-  it('123.456 (fr) -> 123,46 €', () =>{
-    expect(basil.i18n.currency(123.456, { locale: 'fr' })).toBe('123,46 €');
+describe('currency', () => {
+  tests.forEach(t => {
+    let opts = Object.keys(t.options).map(o => {
+      return `${o}: ${t.options[o]}`
+    })
+
+    it(`${t.in} => ${t.out} [${opts}]`, () => {
+      expect(basil.i18n.currency(t.in, t.options)).toBe(t.out);
+    })
   })
 })
